c3a3014d3cd0feeedb5c8aa01129ca6fc7fbe8ba
amixcc01@gmail.com <amixcc01@gmail.com>
2026-04-13 10:59:43 +0800

    chore: update app version to 1.0.0-2604131058

--- lib/utils/app_config.dart
+++ lib/utils/app_config.dart
@@ -25,7 +25,7 @@ class AppConfig {
   static const String dataCenterCode = 'cn';
 
   /// 应用版本号
-  static const String appVersion = '1.0.0-2604101618';
+  static const String appVersion = '1.0.0-2604131058';
 
   /// 默认语言
   static const String defaultLanguage = 'en_US';
